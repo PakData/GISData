ba1807878880a54406734390276b39fd3f16cb0e
hazrmard <ibrahim78786@gmail.com>
2017-05-03 17:27:16 -0500

    added data files + script

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
 # GISData
-Geographical data files for Pakistan
+Geographical data files for Pakistan.
+
+This repository contains shapefiles and TopoJSON files for Pakistan.
+
+
+## Heirarchy
+
+Files contain boundaries on 4 levels:
+
+* 0 - National
+* 1 - Provincial
+* 2 - Division
+* 3 - District
+
+Each file is labeled accoring to it's level for e.g. `PAK-TopoJSON/PAK_adm1.json`
+contains provincial boundaries.
+
+## Updating
+
+To update this data set, first install the `update-data.js` script. You will
+need to have *Node.js* installed.
+
+```
+> npm install .
+```
+
+After installation you can run the script
+
+```
+> node update-data.js
+```
+
+Updates depend on how frequently to original database is changed.
+
+## More
+
+More information on shape files can be found [here][1].
+
+More information on TopoJSON files can be found [here][2].
+
+These files can be visualezed at [mapshaper][4].
+
+
+## Acknowledgements
+
+The data files were obtained from the [Global Administrative Boundaries Database][3].
+
+[1]: https://en.wikipedia.org/wiki/Shapefile
+[2]: https://github.com/topojson/topojson
+[3]: http://gadm.org
+[4]: http://mapshaper.org/
---
+++ update-data.js
@@ -0,0 +1,93 @@
+/*
+This script downloads shapefules from the Global Administrative Boundaries
+Database and converts the .shp files into TopoJSON format which is usable
+by D3.js.
+
+Installation:
+
+Browse to repository's directory, then:
+
+  > nmp install .
+
+Usage:
+
+  > node update-data COUNTRY
+  > node update-data
+
+Where COUNTRY is the country code. If no country code provided, assumed to be
+Pakistan (PAK).
+
+The resulting directory structure is:
+
+  ./
+  COUNTRY-SHP/
+  ....  shapefiles
+  COUNTRY-TopoJSON/
+  ....  json files
+*/
+
+
+'use strict';
+
+var shapefile = require("shapefile");
+var child_process = require("child_process");
+var http = require('http');
+var fs = require('fs');
+var process = require("process");
+var unzip = require("unzip");
+var glob = require("glob");
+
+
+// download a file
+// http://stackoverflow.com/a/22793628/4591810
+var download = function(url, dest, cb, cbArgs) {
+  var file = fs.createWriteStream(dest);
+  var request = http.get(url, function(response) {
+    response.pipe(file);
+    file.on('finish', function() {
+      file.close(cb);
+    });
+  });
+}
+
+// extract .zip files
+var extract = function(fname, dest, cb) {
+  fs.createReadStream(fname)
+    .pipe(unzip.Extract({ path: dest }))
+    .on('close', cb);
+}
+
+
+var preBaseURL = "http://biogeo.ucdavis.edu/data/gadm2.8/shp/";
+var postBaseURL = "_adm_shp.zip"
+
+var country = process.argv.length >= 3 ? process.argv[3].toUpperCase() : 'PAK';
+var fullURL = preBaseURL + country + postBaseURL;
+var command = process.platform === 'win32' ? 'shp2json.cmd ' : 'shp2json ';
+
+var zipFile = country + '.zip';
+var shapeDir = country + '-SHP';
+var jsonDir = country + '-TopoJSON';
+
+
+// download country specific .zip file
+download(fullURL, zipFile, function() {
+
+  // extract to folder containing shape files
+  extract(zipFile, shapeDir, function() {
+
+    // delete .zip file
+    fs.unlinkSync(zipFile);
+    // get list of all .shp files
+    glob(shapeDir + '/*.shp', function(err, matches) {
+
+      // make directory for JSON files
+      fs.existsSync(jsonDir) || fs.mkdirSync(jsonDir);
+      // convert each .shp file to TopoJSON
+      for (var i=0; i < matches.length; i++) {
+        var f = matches[i].slice(shapeDir.length, -3);
+        child_process.exec(command + matches[i] + ' -o ' + jsonDir + '/' + f + 'json');
+      };
+    });
+  });
+});
